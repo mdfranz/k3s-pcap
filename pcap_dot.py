5f94962aaae7d203fb3a1c219bce15a3466a026a
Matt Franz <mdfranz@gmail.com>
2024-11-12 22:29:04 -0500

    Cleanup of enrichment

--- pcap_dot.py
+++ pcap_dot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3                                                                                                                                                                                                                                       
-import sys, pyshark, pickle,csv,time
+import sys, pickle, csv, time
 from pcapkit import IP, extract
 
 def extract_pcap(input_file,in_flows={},debug=True):
@@ -23,31 +23,13 @@ def extract_pcap(input_file,in_flows={},debug=True):
                     print ("New dst",dst_ip)
     return flows                          
 
-
-def shark_pcap(input_file,in_flows={},debug=False):
-    cap = pyshark.FileCapture(input_file)
-    flows = in_flows
-    for e in cap:                                              
-        if 'IP' in e:                          
-            if 'TCP' in e or 'UDP' in e:        
-                (src_eth,src_ip, dst_eth,dst_ip) =  (e.eth.src,e.ip.src,e.eth.dst,e.ip.dst)                                   
-                if src_ip not in flows:
-                    flows[src_ip] = []                         
-                    if debug:
-                        print ("New src",src_ip)
-                if dst_ip not in flows[src_ip]:
-                    flows[src_ip].append(dst_ip)               
-                    if debug:
-                        print ("New dst",dst_ip)
-    return flows                          
-
 def dump_flow(flows):
     print(flows)
     for src in flows.keys():
         print("=== SRC:",src)
         print("  ",flows[src])
 
-def enrich_dict(data,lookup_file):
+def enrich_dict(data,lookup_file,debug=True):
     """
         falco-falcosidekick-746c76f858-qgx96,10.42.1.41
         falco-falcosidekick-ui-7bbfd79c5-282tl,10.42.1.42
@@ -61,18 +43,29 @@ def enrich_dict(data,lookup_file):
             name, ip_address = row
             lookup_table[ip_address] = name
 
-    print(lookup_table)
+    if debug:
+        print ("-------Lookup ---------")
+        print(lookup_table)
 
     for k in data.keys():
-        print(f"Checking {k} in Lookup") 
+        if debug:
+            print(f"Checking {k} in Lookup") 
+
         if k in lookup_table:
-            # Replace keys
-            out_dict[k + "\n("+ lookup_table[k] +")"] = data[k]
-            # inefficient AF
-            for v in data[k]:
-                if v in lookup_table:
-                    data[k].remove(v)
-                    data[k].append(v + "\n("+ lookup_table[v] +")")
+            new_key = lookup_table[k] + "\n("+ k +")"
+            out_dict[new_key] = []
+        else:
+            out_dict[k] = []
+            new_key = k
+
+        for v in data[k]:
+            if debug:
+                print(f"Checking if {v} in Lookup") 
+
+            if v in lookup_table:
+                out_dict[new_key].append(lookup_table[v] +  "\n("+ v +")")
+            else:
+                out_dict[new_key].append(v)
 
     print (out_dict) 
 
@@ -99,13 +92,13 @@ def pickle_load(input_file):
                                                          
 if __name__ == "__main__":        
     if len(sys.argv) < 2:
-        print ("Usage:\n shark.py <pcap> [pickle]")
+        print ("Usage:\n pcap_dot.py <pcap> <services> [pickle file]")
     else:
         # Use a pickle file
         if len (sys.argv) > 3:
-            if os.exists(sys.argv[2]):
-                loaded_flows = pickle_load(sys.argv[2])
+            if os.exists(sys.argv[3]):
+                loaded_flows = pickle_load(sys.argv[3])
         else:
             f = extract_pcap(sys.argv[1])
-            g = enrich_dict(f,"lookup.csv")
+            g = enrich_dict(f,sys.argv[2])
             dict_to_dot(g)
